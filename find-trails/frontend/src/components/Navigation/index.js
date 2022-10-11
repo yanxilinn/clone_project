da2c9407e150e340ab4b6178500a82435e36a836
Yanxi_Lin <yanxilinnn@gmail.com>
2022-10-11 17:38:14 -0400

    update

--- find-trails/frontend/src/components/Navigation/index.js
+++ find-trails/frontend/src/components/Navigation/index.js
@@ -23,38 +23,38 @@ function Navigation() {
       <div className='login'>
         <Link id='login' to="/login">Log In</Link>
       </div>
-        {/* <LoginFormModal /> */}
       </>
     );
   }
 
   return (
-    <div className='nav'>
-      <div className='nav-left'>
-          {/* <div>
-            explore
-          </div>
-          <div>
-            community
-          </div>
-          <div>
-            saved
-          </div>
-          <div>
-            shop
-          </div> */}
-      </div>
-        <div className='logo'>
-          <Link id="logo-link" to='/'>
-            <img id='logo-img-home' src={logo2} alt="logo"></img> 
-          </Link>
+    <>
+      <div className='nav'>
+        <div className='nav-left'>
+            {/* <div>
+              explore
+            </div>
+            <div>
+              community
+            </div>
+            <div>
+              saved
+            </div>
+            <div>
+              shop
+            </div> */}
         </div>
+          <div className='logo'>
+            <Link id="logo-link" to='/'>
+              <img id='logo-img-home' src={logo2} alt="logo"></img> 
+            </Link>
+          </div>
 
-        <div className='nav-right'>
-            {sessionLinks}
+          <div className='nav-right'>
+              {sessionLinks}
+          </div>
         </div>
-    </div>
-        
+      </>
   );
 }
 
