45b3ea7e6a37f629d7572c81918e507af03f2834
Yanxi_Lin <yanxilinnn@gmail.com>
2022-10-13 11:19:30 -0400

    update

--- find-trails/db/seeds.rb
+++ find-trails/db/seeds.rb
@@ -26,15 +26,15 @@ ApplicationRecord.transaction do
     )
   
     # More users
-    10.times do 
-      User.create!({
-        first_name: Faker::Name.first_name,
-        last_name: Faker::Name.last_name,
-        email: Faker::Internet.unique.email,
-        password: 'password',
-        bio: 'I am demo user :)'
-      }) 
-    end
+    # 10.times do 
+    #   User.create!({
+    #     first_name: Faker::Name.first_name,
+    #     last_name: Faker::Name.last_name,
+    #     email: Faker::Internet.unique.email,
+    #     password: 'password',
+    #     bio: 'I am demo user :)'
+    #   }) 
+    # end
   
     puts "Done!"
   end
